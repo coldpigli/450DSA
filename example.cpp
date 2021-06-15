8703e22bd7c8dc80efedff98f9de85ecd4c31d6d
coldpigli <piyushdas98@gmail.com>
2021-06-15 13:46:52 +0530

    removeDuplicatesLeetCode

---
+++ example.cpp
@@ -0,0 +1,44 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+
+int removeDuplicates(vector<int>& nums) {
+        
+        set<int, greater<int>> s1;
+        for(auto it: nums){
+            s1.insert(it);
+        }
+
+
+        
+        nums.clear();
+        for(auto it: s1){
+        	cout<<it<<" ";
+            nums.push_back(it);
+        }
+        
+        return nums.size();
+        
+    }
+
+
+
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+		// for getting input from input.txt
+		freopen("input.txt","r",stdin);
+		//for writing output to output.txt
+		freopen("output.txt","w",stdout);
+	#endif
+		
+	//**********************************************//
+		vector<int> arr = {1,1,2,3,4,4,5};
+		int k = removeDuplicates(arr);
+		cout<<k;
+
+
+	//**********************************************//
+	return 0;
+}
