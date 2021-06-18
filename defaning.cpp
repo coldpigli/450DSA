c3c4c1d2a8cee7d41e0220eab44cd855970b7752
coldpigli <piyushdas98@gmail.com>
2021-06-18 19:45:03 +0530

    Defanging Leetcode

---
+++ defaning.cpp
@@ -0,0 +1,41 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+	string defanging(string s){
+		string result = "";
+		for(int i = 0; i<s.size(); i++){
+			if(s[i]=='.')
+			{
+				result.append("[.]");
+			}
+			else
+			{
+				result.push_back(s[i]);
+			}
+		}
+		return result;
+	}
+
+
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+		// for getting input from input.txt
+		freopen("input.txt","r",stdin);
+		//for writing output to output.txt
+		freopen("output.txt","w",stdout);
+	#endif
+		
+	//**********************************************//
+
+		string s = "1.1.1.1";
+		string res;
+		res = defanging(s);
+		cout<<res;
+
+        
+
+	//**********************************************//
+	return 0;
+}
